d9406f6ca1fb9723d8a3b84db30a37d5626fcf65
x86byte <111459558+x86byte@users.noreply.github.com>
2026-05-25 11:18:24 +0100

    verify multi-string _OBF macro
    updated test case to use the _OBF macro. Verified that strings.exe/strings cannot find literals in the compiled binary

--- EXAMPLES/sbox_test.cpp
+++ EXAMPLES/sbox_test.cpp
@@ -2,26 +2,28 @@
 
 string enc(vector<string> &s, sbox sx)
 {
-   	cout << "[#] Encrypting....\n";
-    return (sx.encode(s));
+      cout << "[#] Encrypting....\n";
+      return (sx.encode(s));
 }
 
 vector<string> dec(string s, sbox sx)
 {
-   	cout << "[#] Decrypting....\n";
-    return (sx.decode(s));
+      cout << "[#] Decrypting....\n";
+      return (sx.decode(s));
 }
 
 int main()
-{
-  	sbox sx;
-  	vector<string> s;
-  	s.push_back("ama obfuscation addicted");
+{  
+    sbox sx;
+    vector<string> s;
+    s.push_back(_OBF("ama obfuscation addicted"));
+    s.push_back(_OBF("x86byte logic is invisible"));
+    s.push_back(_OBF("static analysis is for losers?"));
     auto res = enc(s, sx);
-  	cout << "	[+] Encrypted strings :\n	    - " << res << endl;
-  	auto vs = dec(res, sx);
-  	cout << "	[+] Decrypted strings : " << endl;
-  	for(auto ss : vs)
-  		cout << "	    - " << ss << endl;
-  	return 0;
+    cout << "	[+] Encrypted strings :\n	    - " << res << endl;
+    auto recovered = dec(res, sx);
+    cout <<  "	[+] Decrypted strings :\n";
+    for(auto& str : recovered)
+    	cout << "	    - " << str << endl;
+    return 0;
 }
